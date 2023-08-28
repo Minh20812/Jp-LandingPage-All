f7158d882ed36e59a15e618589b1fad59ec04efb
Minh (minova) <92965110+Minh20812@users.noreply.github.com>
2023-08-28 11:07:39 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector(".site_wrap");
+const menuButton = document.querySelector(".navbar_icons");
+const overlay = document.querySelector("#overlay");
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("navbar_open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.toggle("navbar_open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
